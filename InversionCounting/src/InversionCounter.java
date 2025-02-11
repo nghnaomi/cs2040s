7ecf53f306a8a769e22ca46ee4a9c3de1eaab528
naomi <163950105+nghnaomi@users.noreply.github.com>
2025-02-11 11:27:02 +0800

    Add files via upload

---
+++ InversionCounting/src/InversionCounter.java
@@ -0,0 +1,15 @@
+class InversionCounter {
+
+    public static long countSwaps(int[] arr) {
+        return 0;
+    }
+
+    /**
+     * Given an input array so that arr[left1] to arr[right1] is sorted and arr[left2] to arr[right2] is sorted
+     * (also left2 = right1 + 1), merges the two so that arr[left1] to arr[right2] is sorted, and returns the
+     * minimum amount of adjacent swaps needed to do so.
+     */
+    public static long mergeAndCount(int[] arr, int left1, int right1, int left2, int right2) {
+        return 0;
+    }
+}
